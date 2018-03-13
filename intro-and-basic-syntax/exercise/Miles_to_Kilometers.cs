75512a120d66524babf0e14bf576bc896adc6f1f
ctelio <ctelio@mail.bg>
2018-03-13 11:34:49 +0200

    ctelio - Member 3 - solution for Problem № 3

---
+++ intro-and-basic-syntax/exercise/Miles_to_Kilometers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Debit_Card_Number
+{
+    class Miles_to_Kilometers
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double a = double.Parse(Console.ReadLine());
+            //calc
+            double b = a * 1.60934;
+            //output
+            Console.Write("{0:F2}", b);
+            Console.ReadLine();
+        }
+    }
+}
