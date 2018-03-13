acf578081cf1c60a6a7ae6b793398981f3e83bc9
vladevdev <vladevdev@gmail.com>
2018-03-13 20:41:56 +0200

    Vladevdev documents the code of Solution of Problem 3;

--- intro-and-basic-syntax/exercise/Miles_to_Kilometers.cs
+++ intro-and-basic-syntax/exercise/Miles_to_Kilometers.cs
@@ -6,15 +6,13 @@ namespace Debit_Card_Number
     {
         static void Main(string[] args)
         {
-            //input
+
             //We recieve from the Consone miles as double
             double a = double.Parse(Console.ReadLine());
-
-            //calculation
+            
             //We convert miles to kilometers
             double b = a * 1.60934;
 
-            //output
             //We print on console kilometers, formated to the second character
             Console.Write("{0:F2}", b);
             Console.ReadLine();
